6c9491e76c4f1aed80f87485fdbd92adc2cae3ee
omar chavez <omarchavez@omars-MacBook-Pro.local>
2021-06-03 07:53:19 -0500

    pulling in files from other branch to resolve merge conflicts

--- bartpy/bartpy/samplers/unconstrainedtree/likihoodratio.py
+++ bartpy/bartpy/samplers/unconstrainedtree/likihoodratio.py
@@ -35,31 +35,7 @@ def log_grow_ratio(combined_node: LeafNode, left_node: LeafNode, right_node: Lea
     return output
 
 
-def log_grow_ratio_cgm_g_(combined_node: LeafNode, left_node: LeafNode, right_node: LeafNode, sigma: Sigma, sigma_mu: float):
-
-    var = np.power(sigma.current_value(), 2)
-    var_mu = np.power(sigma_mu, 2)
-    n = combined_node.data.X.n_obsv
-    n_l = left_node.data.X.n_obsv
-    n_r = right_node.data.X.n_obsv
-
-    first_term = (var * (var + n * sigma_mu)) / ((var + n_l * var_mu) * (var + n_r * var_mu))
-    first_term = np.log(np.sqrt(first_term))
-
-    combined_y_sum = combined_node.data.y.summed_y()
-    left_y_sum = left_node.data.y.summed_y()
-    right_y_sum = right_node.data.y.summed_y()
-
-    left_resp_contribution = np.square(left_y_sum) / (var + n_l * sigma_mu)
-    right_resp_contribution = np.square(right_y_sum) / (var + n_r * sigma_mu)
-    combined_resp_contribution = np.square(combined_y_sum) / (var + n * sigma_mu)
-
-    resp_contribution = left_resp_contribution + right_resp_contribution - combined_resp_contribution
-    output = first_term + ((var_mu / (2 * var)) * resp_contribution)
-
-    return output
-
-def log_grow_ratio_cgm_g(combined_node: LeafNode, left_node: LeafNode, right_node: LeafNode, sigma: Sigma, sigma_mu: float):
+def log_grow_ratio_cgm_g(combined_node: LeafNode, left_node: LeafNode, right_node: LeafNode, sigma: Sigma, sigma_mu: float, mu_g: float):
     
     var = np.power(sigma.current_value(), 2)
     var_mu = np.power(sigma_mu, 2)
@@ -77,26 +53,45 @@ def log_grow_ratio_cgm_g(combined_node: LeafNode, left_node: LeafNode, right_nod
     A_right = 1/var_mu + sum_sigma_g_i_sqr_right
     A_combined = 1/var_mu + sum_sigma_g_i_sqr_combined
 
-    first_term = .5 * (-np.log(var_mu) + np.log(A_combined) - np.log(A_left) - np.log(A_right))
+    #first_term = .5 * (np.log(var_mu) + np.log(A_combined) - np.log(A_left) - np.log(A_right))
+    first_term = (
+        -.5 * (np.log(var_mu) + 
+         .5 * np.log(A_combined) - 
+         .5 * np.log(A_left) - 
+         .5 * np.log(A_right))
+    )
     
     y_tilde_g_i = combined_node.data.y.values
     y_tilde_g_i_over_var_i = y_tilde_g_i/sigma_g_i_sqr
     
-    A_left_left_sum = (1/A_left)*np.sum( (~left_node.data.mask).astype(int) * y_tilde_g_i_over_var_i)**2
-    A_right_right_sum = (1/A_right)*np.sum((~right_node.data.mask).astype(int) * y_tilde_g_i_over_var_i)**2
-    A_combined_combined_sum = (1/A_combined)*np.sum((~combined_node.data.mask).astype(int) * y_tilde_g_i_over_var_i)**2
+    A_left_left_sum = (1/A_left)*(
+        np.sum(
+            (~left_node.data.mask).astype(int) * y_tilde_g_i_over_var_i 
+        ) + mu_g/var_mu
+    )**2
+    A_right_right_sum = (1/A_right)*(
+        np.sum(
+            (~right_node.data.mask).astype(int) * y_tilde_g_i_over_var_i 
+        ) + mu_g/var_mu
+    )**2
+    A_combined_combined_sum = (1/A_combined)*(
+        np.sum(
+            (~combined_node.data.mask).astype(int) * y_tilde_g_i_over_var_i
+        ) + mu_g/var_mu
+    )**2
     
     left_resp_contribution = 0.5 *  A_left_left_sum
     right_resp_contribution = 0.5 *  A_right_right_sum
     combined_resp_contribution = 0.5 *  A_combined_combined_sum
     
-    resp_contribution = left_resp_contribution + right_resp_contribution - combined_resp_contribution
+    resp_contribution = -.5*(mu_g**2)/var_mu + left_resp_contribution + right_resp_contribution - combined_resp_contribution
 
     output = first_term + resp_contribution
-
+    #print("G log grow ratio:", output)
     return output
 
-def log_grow_ratio_cgm_h(combined_node: LeafNode, left_node: LeafNode, right_node: LeafNode, sigma: Sigma, sigma_mu: float):
+
+def log_grow_ratio_cgm_h(combined_node: LeafNode, left_node: LeafNode, right_node: LeafNode, sigma: Sigma, sigma_mu: float, mu_h: float):
     
     var = np.power(sigma.current_value(), 2)
     var_mu = np.power(sigma_mu, 2)
@@ -104,7 +99,8 @@ def log_grow_ratio_cgm_h(combined_node: LeafNode, left_node: LeafNode, right_nod
     W=combined_node.data.W.values
     p=combined_node.data.p.values
     
-    sigma_h_i_sqr = var * ( W/(p**2) + (1-W)/((1-p)**2) )
+    #sigma_h_i_sqr = var * ( W/(p**2) + (1-W)/((1-p)**2) )
+    sigma_h_i_sqr = var /((p**2) * (1-p)**2) 
     
     sum_sigma_h_i_sqr_left = np.sum( (~left_node.data.mask).astype(int) * 1./sigma_h_i_sqr)
     sum_sigma_h_i_sqr_right = np.sum( (~right_node.data.mask).astype(int) * 1./sigma_h_i_sqr)
@@ -114,23 +110,41 @@ def log_grow_ratio_cgm_h(combined_node: LeafNode, left_node: LeafNode, right_nod
     A_right = 1/var_mu + sum_sigma_h_i_sqr_right
     A_combined = 1/var_mu + sum_sigma_h_i_sqr_combined
 
-    first_term = .5 * (-np.log(var_mu) + np.log(A_combined) - np.log(A_left) - np.log(A_right))
+    #first_term = .5 * (np.log(var_mu) + np.log(A_combined) - np.log(A_left) - np.log(A_right))
+    first_term = (
+        -.5 * (np.log(var_mu) + 
+         .5 * np.log(A_combined) - 
+         .5 * np.log(A_left) - 
+         .5 * np.log(A_right))
+    )
     
     y_tilde_h_i = combined_node.data.y.values
     y_tilde_h_i_over_var_i = y_tilde_h_i/sigma_h_i_sqr
     
-    A_left_left_sum = (1/A_left)*np.sum( (~left_node.data.mask).astype(int) * y_tilde_h_i_over_var_i)**2
-    A_right_right_sum = (1/A_right)*np.sum((~right_node.data.mask).astype(int) * y_tilde_h_i_over_var_i)**2
-    A_combined_combined_sum = (1/A_combined)*np.sum((~combined_node.data.mask).astype(int) * y_tilde_h_i_over_var_i)**2
+    A_left_left_sum = (1/A_left)*(
+        np.sum( 
+            (~left_node.data.mask).astype(int) * y_tilde_h_i_over_var_i
+        ) + mu_h/var_mu
+    )**2
+    A_right_right_sum = (1/A_right)*(
+        np.sum(
+            (~right_node.data.mask).astype(int) * y_tilde_h_i_over_var_i
+        ) + mu_h/var_mu
+    )**2
+    A_combined_combined_sum = (1/A_combined)*(
+        np.sum(
+            (~combined_node.data.mask).astype(int) * y_tilde_h_i_over_var_i
+        ) + mu_h/var_mu
+    )**2
     
     left_resp_contribution = 0.5 *  A_left_left_sum
     right_resp_contribution = 0.5 *  A_right_right_sum
     combined_resp_contribution = 0.5 *  A_combined_combined_sum
     
-    resp_contribution = left_resp_contribution + right_resp_contribution - combined_resp_contribution
+    resp_contribution = -.5*(mu_h**2)/var_mu +  left_resp_contribution + right_resp_contribution - combined_resp_contribution
 
     output = first_term + resp_contribution
-
+    #print("H log grow ratio:", output)
     return output
 
 class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
@@ -167,6 +181,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
             return output
 
     def log_tree_ratio_cgm(self, model: ModelCGM, tree: Tree, mutation: TreeMutation):
+        #print("enter bartpy/bartpy/samplers/unconstrainedtree/likihoodratio.py UniformTreeMutationLikihoodRatio  log_tree_ratio_cgm")
 
         if mutation.kind == "grow":
             mutation: GrowMutation = mutation
@@ -176,6 +191,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
             mutation: PruneMutation = mutation
             output = self.log_tree_ratio_prune_cgm(model, mutation)
             return output
+        #print("exit bartpy/bartpy/samplers/unconstrainedtree/likihoodratio.py UniformTreeMutationLikihoodRatio  log_tree_ratio_cgm")
 
     def log_likihood_ratio(self, model: Model, tree: Tree, proposal: TreeMutation):
 
@@ -198,7 +214,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
             return output
         if proposal.kind == "prune":
             proposal: PruneMutation = proposal
-            output = self.log_likihood_ratio_prune_cgm_h(model, proposal)
+            output = self.log_likihood_ratio_prune_cgm_g(model, proposal) ###########this was an h!!!!!
             return output
         else:
             raise NotImplementedError("Only prune and grow mutations supported")
@@ -234,7 +250,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
             proposal.existing_node, 
             proposal.updated_node.left_child, 
             proposal.updated_node.right_child,
-            model.sigma, model.sigma_m)
+            model.sigma, model.sigma_g, model.mu_g)
 
         return output
 
@@ -245,7 +261,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
             proposal.existing_node, 
             proposal.updated_node.left_child, 
             proposal.updated_node.right_child, 
-            model.sigma, model.sigma_m)
+            model.sigma, model.sigma_h, model.mu_h)
 
         return output
     
@@ -267,7 +283,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
             proposal.updated_node, 
             proposal.existing_node.left_child, 
             proposal.existing_node.right_child, 
-            model.sigma, model.sigma_m)
+            model.sigma, model.sigma_g, model.mu_g)
 
         return output
 
@@ -278,7 +294,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
             proposal.updated_node, 
             proposal.existing_node.left_child, 
             proposal.existing_node.right_child, 
-            model.sigma, model.sigma_m)
+            model.sigma, model.sigma_h, model.mu_h)
 
         return output
 
@@ -334,6 +350,7 @@ class UniformTreeMutationLikihoodRatio(TreeMutationLikihoodRatio):
         prob_right_not_split = log_probability_node_not_split(model, proposal.updated_node.right_child)
         prob_updated_node_split = log_probability_node_split(model, proposal.updated_node)
         prob_chosen_split = log_probability_split_within_tree(tree, proposal)
+        
         numerator = prob_left_not_split + prob_right_not_split + prob_updated_node_split + prob_chosen_split
         output = numerator - denominator
 
@@ -424,8 +441,8 @@ def log_probability_split_within_tree(tree: Tree, mutation: GrowMutation) -> flo
     """
 
     prob_node_chosen_to_split_on = - np.log(n_splittable_leaf_nodes(tree))
-    prob_split_chosen = log_probability_split_within_node(mutation)
-    output = prob_node_chosen_to_split_on + prob_split_chosen
+    prob_split_chosen            = log_probability_split_within_node(mutation)
+    output                       = prob_node_chosen_to_split_on + prob_split_chosen
 
     return output
 
@@ -438,9 +455,9 @@ def log_probability_split_within_node(mutation: GrowMutation) -> float:
     log(P(splitting_value | splitting_variable, node, grow) * P(splitting_variable | node, grow))
     """
 
-    prob_splitting_variable_selected = - np.log(mutation.existing_node.data.X.n_splittable_variables)
-    splitting_variable = mutation.updated_node.most_recent_split_condition().splitting_variable
-    splitting_value = mutation.updated_node.most_recent_split_condition().splitting_value
+    prob_splitting_variable_selected    = - np.log(mutation.existing_node.data.X.n_splittable_variables)
+    splitting_variable                  = mutation.updated_node.most_recent_split_condition().splitting_variable
+    splitting_value                     = mutation.updated_node.most_recent_split_condition().splitting_value
     prob_value_selected_within_variable = np.log(
         mutation.existing_node.data.X.proportion_of_value_in_variable(
             splitting_variable, splitting_value
@@ -452,11 +469,15 @@ def log_probability_split_within_node(mutation: GrowMutation) -> float:
 
 
 def log_probability_node_split(model: Model, node: TreeNode):
+    #print("split node depth:", node.depth)
     output = np.log(model.alpha * np.power(1 + node.depth, -model.beta))
+    #print("ln(P(node depth))=", output)
     return output
 
 
 def log_probability_node_not_split(model: Model, node: TreeNode):
+    #print("split node depth:", node.depth)
     output = np.log(1. - model.alpha * np.power(1 + node.depth, -model.beta))
+    #print("ln(1-P(node depth))=", output)
     return output
 
